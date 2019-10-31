6699c515bf6508676060da5d4328cfadc13a877a
Abror Ilkhamov <AIlkhamov@spartaglobal.com>
2019-10-31 09:57:43 +0000

    Done

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,28 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+  action :update
+end
+
+package "python" do
+  action :install
+end
+
+ package 'python-pip'  # package default will install
+
+
+
+#### requests==2.3.0  #######
+execute 'install requests==2.3.0' do
+  command 'pip install requests==2.3.0'
+end
+
+execute 'install pytest==2.5.2' do
+  command 'pip install pytest==2.5.2'
+end
+
+# ##### Coverage pytest-cov==1.6 #######
+execute 'install pytest-cov==1.6' do
+  command 'pip install pytest-cov==1.6'
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -17,13 +17,24 @@ describe 'python::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
+    ####### updating all sources in ubuntu ##########
+ it 'should run apt-get update ' do
+   expect(chef_run).to update_apt_update 'update_sources'
+ end
+
+ it 'should install python package' do
+   expect(chef_run).to install_package 'python'
+ end
+
+ it 'should install Python package management pip' do
+   expect(chef_run).to install_package 'python-pip'
+end
+
+
+it 'should install requests plugin using pip management' do
+  expect(chef_run).to run_execute ('install requests==2.3.0')
+end
 end
